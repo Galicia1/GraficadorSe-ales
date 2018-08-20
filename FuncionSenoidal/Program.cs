4defe676bfdb0e1cf802c0e32bc91fbd506a1290
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-20 09:42:14 -0700

    Agregar archivos de proyecto.

---
+++ FuncionSenoidal/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FuncionSenoidal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SeñalSenoidal señal = new SeñalSenoidal();
+
+            señal.Amplitud = 5;
+            //i +=1;
+            //i = i+1;
+            for(double i=0.0; i<=1; i+=0.05)
+            {
+                Console.WriteLine(señal.evaluar(i));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
